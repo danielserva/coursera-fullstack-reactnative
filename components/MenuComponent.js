19f0c47382f41db2366755a2147c7262eb9dafd7
Sopa <danielserva@gmail.com>
2020-12-27 02:40:20 +0100

    Components Part 1

---
+++ components/MenuComponent.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import { View, FlatList } from 'react-native';
+import { Avatar, ListItem } from 'react-native-elements';
+
+function Menu(props) {
+
+    const renderMenuItem = ({item, index}) => {
+
+        return (
+            <ListItem bottomDivider>
+            <Avatar source={require('./images/uthappizza.png')} />
+            <ListItem.Content>
+              <ListItem.Title>{item.name}</ListItem.Title>
+              <ListItem.Subtitle>{item.description}</ListItem.Subtitle>
+            </ListItem.Content>
+            <ListItem.Chevron />
+          </ListItem>
+        );
+    };
+
+    return (
+            <FlatList 
+                data={props.dishes}
+                renderItem={renderMenuItem}
+                keyExtractor={item => item.id.toString()}
+                />
+    );
+}
+
+
+export default Menu;
