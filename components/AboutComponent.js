95039677000b2fd374b1bb06601f22dfdfe9f629
Sopa <danielserva@gmail.com>
2021-01-02 19:09:28 +0100

    Is loading

--- components/AboutComponent.js
+++ components/AboutComponent.js
@@ -3,6 +3,7 @@ import { Text, ScrollView, FlatList  } from 'react-native';
 import { Avatar, Card, ListItem } from 'react-native-elements';
 import { connect } from 'react-redux';
 import { baseUrl } from '../shared/baseUrl';
+import { Loading } from './LoadingComponent';
 
 const mapStateToProps = state => {
   return {
@@ -26,28 +27,53 @@ class About extends Component {
       );
     };
 
-    return (
-    <ScrollView>
-      <Card style={{padding:0}}>
-        <Card.Title>Our History</Card.Title>
-        <Card.Divider></Card.Divider>
-        <Text style={{margin: 10}}> Started in 2010, Ristorante con Fusion quickly established itself as a culinary icon par excellence in Hong Kong. With its unique brand of world fusion cuisine that can be found nowhere else, it enjoys patronage from the A-list clientele in Hong Kong.  Featuring four of the best three-star Michelin chefs in the world, you never know what will arrive on your plate the next time you visit us.
-        </Text>
-        <Text style={{margin: 10}}> The restaurant traces its humble beginnings to The Frying Pan, a successful chain started by our CEO, Mr. Peter Pan, that featured for the first time the world's best cuisines in a pan.</Text>
-      </Card>
+    const History = () => {
+      return (
+        <Card style={{padding:0}}>
+          <Card.Title>Our History</Card.Title>
+          <Card.Divider></Card.Divider>
+          <Text style={{margin: 10}}> Started in 2010, Ristorante con Fusion quickly established itself as a culinary icon par excellence in Hong Kong. With its unique brand of world fusion cuisine that can be found nowhere else, it enjoys patronage from the A-list clientele in Hong Kong.  Featuring four of the best three-star Michelin chefs in the world, you never know what will arrive on your plate the next time you visit us.
+          </Text>
+          <Text style={{margin: 10}}> The restaurant traces its humble beginnings to The Frying Pan, a successful chain started by our CEO, Mr. Peter Pan, that featured for the first time the world's best cuisines in a pan.</Text>
+        </Card>
+      );
+    }
 
-      <Card style={{padding:0}}>
-        <Card.Title>Corporate Leadership</Card.Title>
-        <Card.Divider></Card.Divider>
+    if (this.props.leaders.isLoading){
+      return(
+        <ScrollView>
+          <History />
+          <Card style={{padding:0}}>
+            <Loading />
+          </Card>
+        </ScrollView>
+      );
+    } else if (this.props.leaders.errMess) {
+      return(
+        <ScrollView>
+          <History />
+          <Card style={{padding:0}}>
+          <Card.Title>Corporate Leadership</Card.Title>
+          <Text style={{margin: 10}}>{this.props.leaders.errMess}</Text>
+          </Card>
+        </ScrollView>
+      );
+    } else {
 
-        <FlatList 
-          data={this.props.leaders.leaders}
-          renderItem={renderLeaderItem}
-          keyExtractor={item => item.id.toString()}
-        />
-      </Card>
-
-    </ScrollView>)
+      return (
+        <ScrollView>
+          <History />
+          <Card style={{padding:0}}>
+            <Card.Title>Corporate Leadership</Card.Title>
+            <Card.Divider></Card.Divider>
+            <FlatList 
+              data={this.props.leaders.leaders}
+              renderItem={renderLeaderItem}
+              keyExtractor={item => item.id.toString()}
+            />
+          </Card>
+        </ScrollView>);
+      }
   }
 }
 export default connect(mapStateToProps)(About);
--- components/HomeComponent.js
+++ components/HomeComponent.js
@@ -3,6 +3,7 @@ import { View, Text, ScrollView } from 'react-native';
 import { Card } from 'react-native-elements';
 import { connect } from 'react-redux';
 import { baseUrl } from '../shared/baseUrl';
+import { Loading } from './LoadingComponent';
 
 const mapStateToProps = state => {
   return {
@@ -15,25 +16,36 @@ const mapStateToProps = state => {
 
 function RenderItem(props){
   const item = props.item;
-  if (item != null){
-    return(
-      <Card style={{padding:0}}>
-        <Card.Image style={{padding:0,alignItems: 'center',justifyContent:'center'}} source={{uri: baseUrl + item.image}}>
-          <Text style={{margin: 10, color:'white'}}>
-            {item.name}
+  if (props.isLoading){
+    return (
+      <Loading />
+    );  
+  } else if (props.errMess) {
+    return (
+      <View>
+        <Text>{props.errMess}</Text>
+      </View>)
+  }else {
+    if (item != null){
+      return(
+        <Card style={{padding:0}}>
+          <Card.Image style={{padding:0,alignItems: 'center',justifyContent:'center'}} source={{uri: baseUrl + item.image}}>
+            <Text style={{margin: 10, color:'white'}}>
+              {item.name}
+            </Text>
+            <Text style={{margin: 10, color:'white'}}>
+              {item.designation}
+            </Text>
+          </Card.Image>
+          <Text style={{margin: 10}}>
+            {item.description}
           </Text>
-          <Text style={{margin: 10, color:'white'}}>
-            {item.designation}
-          </Text>
-        </Card.Image>
-        <Text style={{margin: 10}}>
-          {item.description}
-        </Text>
-      </Card>
-    );
+        </Card>
+      );
 
-  } else {
-    return (<View></View>);
+    } else {
+      return (<View></View>);
+    }
   }
 }
 
@@ -46,9 +58,15 @@ class Home extends Component {
   render() {
     return(
       <ScrollView>
-        <RenderItem item={this.props.dishes.dishes.filter((dish) => dish.featured)[0]} />
-        <RenderItem item={this.props.promotions.promotions.filter((promotion) => promotion.featured)[0]} />
-        <RenderItem item={this.props.leaders.leaders.filter((leader) => leader.featured)[0]} />
+        <RenderItem item={this.props.dishes.dishes.filter((dish) => dish.featured)[0]}
+          isLoading={this.props.dishes.isLoading}
+          errMess={this.props.dishes.errMess} />
+        <RenderItem item={this.props.promotions.promotions.filter((promotion) => promotion.featured)[0]} 
+          isLoading={this.props.promotions.isLoading}
+          errMess={this.props.promotions.errMess} />
+        <RenderItem item={this.props.leaders.leaders.filter((leader) => leader.featured)[0]} 
+          isLoading={this.props.leaders.isLoading}
+          errMess={this.props.leaders.errMess} />
       </ScrollView>
     );
   }
